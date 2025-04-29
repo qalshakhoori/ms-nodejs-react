847c1f04ab54f16800458a603d0f053658e25c26
Qassim Alshakhoori <qalshakhoori>
2025-04-29 14:11:21 +0300

    event loss handling

--- blog-ms/comments/index.js
+++ blog-ms/comments/index.js
@@ -22,25 +22,48 @@ app.post('/posts/:id/comments', (req, res) => {
   const { id } = req.params;
 
   const comments = commentsByPostId[id] || [];
+  const commentToAdd = {
+    id: commentId,
+    content,
+    status: 'pending',
+  };
 
-  comments.push({ id: commentId, content });
+  comments.push(commentToAdd);
 
   commentsByPostId[id] = comments;
 
   axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
-      id: commentId,
-      content,
       postId: id,
+      ...commentToAdd,
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        postId,
+        id,
+        status,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
