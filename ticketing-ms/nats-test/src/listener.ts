9bdb1d1b7d5a8e1aa00f9ae7032dff2803e234be
Qassim Alshakhoori <qalshakhoori>
2025-07-10 12:22:49 +0300

    Event definitions in common module

--- ticketing-ms/nats-test/src/listener.ts
+++ ticketing-ms/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -14,28 +15,7 @@ stan.on('connect', () => {
     console.log('NATS connection closed!');
     process.exit();
   });
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('listener');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'listener-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
--- ticketing-ms/nats-test/src/publisher.ts
+++ ticketing-ms/nats-test/src/publisher.ts
@@ -1,21 +1,18 @@
 import nats from 'node-nats-streaming';
-
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+  await publisher.publish({
     id: '123',
     title: 'concert',
     price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published', 'ticket:created', data);
   });
 });
