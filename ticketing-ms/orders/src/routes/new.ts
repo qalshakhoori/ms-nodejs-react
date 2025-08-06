4d0eadfd86169d1229b448ca1b38c4fe0e01de41
Qassim Alshakhoori <qalshakhoori>
2025-08-06 10:44:00 +0300

    expiration service

--- ticketing-ms/orders/src/routes/new.ts
+++ ticketing-ms/orders/src/routes/new.ts
@@ -15,7 +15,7 @@ import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 minutes
+const EXPIRATION_WINDOW_SECONDS = 1 * 60; // 15 minutes
 
 router.post(
   '/api/orders',
