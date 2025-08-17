2c73e081a94ea7161096c9bc80af78500189713e
Qassim Alshakhoori <qalshakhoori>
2025-08-17 11:08:41 +0300

    payments service

--- ticketing-ms/client/pages/index.js
+++ ticketing-ms/client/pages/index.js
@@ -1,18 +1,52 @@
-import buildClient from '../api/buildClient';
+import Link from 'next/link';
 
-const Landing = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are not signed in</h1>
+const Landing = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link
+            className='nav-link'
+            href='/tickets/[ticketId]'
+            as={`/tickets/${ticket.id}`}
+          >
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-Landing.getInitialProps = async (context) => {
-  console.log('Landing Page Initial Props');
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+Landing.getInitialProps = async (context, client, currentUser) => {
+  try {
+    const { data } = await client.get('/api/tickets');
+
+    return { tickets: data };
+  } catch (error) {
+    console.error('Error fetching tickets:', error);
+    return { tickets: [] };
+  }
+  // const { data } = await client.get('/api/tickets');
+
+  // return { tickets: data };
 };
 
 export default Landing;
--- ticketing-ms/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing-ms/orders/src/events/listeners/expiration-complete-listener.ts
@@ -14,7 +14,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
   queueGroupName: string = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
